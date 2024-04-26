2f8c2a08cf1893675746da328101f9e6696df60a
Sonu <Sonukr@users.noreply.github.com>
2024-04-26 20:55:39 +0530

    added next-cloudinary for better images

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,14 +1,28 @@
 import MediaGallery from '@/components/MediaGallery';
+import {v2 as cloudinary} from 'cloudinary';
+import { cloudinaryCloudName, cloudinaryApiKey, cloudinaryApiSecret  } from '../../lib/config';
+
+
+cloudinary.config({
+  // cloud_name: cloudinaryCloudName ,
+  // api_key: cloudinaryApiKey,
+  // api_secret: cloudinaryApiSecret,
+  // secure: true,
+
+  cloud_name: 'dvtzeylhv',
+  api_key: '597931921642148',
+  api_secret: '9RZnKJEPYj5gvFkuhjZV1oEtRSE',
+  secure: true,
+})
 
 export default async function Home() {
+
+  const {resources: photos} = await cloudinary.api.resources();
+  console.log(photos)
   return (
     <div className="h-full mt-6">
       <MediaGallery
-        resources={[
-          {
-            id: 'my-image'
-          }
-        ]}
+        resources={photos}
       />
     </div>
   )
---
+++ src/lib/config.js
@@ -0,0 +1,3 @@
+export const cloudinaryCloudName = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
+export const cloudinaryApiKey = process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY;
+export const cloudinaryApiSecret = process.env.CLOUDINARY_API_SECRET;
